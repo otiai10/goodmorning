f93e45dfa7628d8a00db7e55a5122ab01272e89f
otiai10 <otiai10@gmail.com>
2013-08-11 21:44:59 +0900

    Minimum Clock

--- goodmorning/GoodMViewController.h
+++ goodmorning/GoodMViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface GoodMViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *now;
 
 @end
--- goodmorning/GoodMViewController.m
+++ goodmorning/GoodMViewController.m
@@ -9,7 +9,8 @@
 #import "GoodMViewController.h"
 
 @interface GoodMViewController ()
-
+-(void)updateNowDisplay;
+-(void)runTheClock;
 @end
 
 @implementation GoodMViewController
@@ -18,8 +19,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.now.text = @"FUGA";
+    [self runTheClock];
 }
 
+-(void)updateNowDisplay
+{
+    /* 日付を取得する */
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YYYY/MM/dd HH:mm:ss"];
+    NSDate *date = [NSDate date];
+    NSString *dateStr = [formatter stringFromDate:date];
+    
+    self.now.text = dateStr;
+}
+-(void)runTheClock
+{
+    [NSTimer scheduledTimerWithTimeInterval: 0.5
+											 target: self
+										   selector:@selector(updateNowDisplay)
+										   userInfo: nil
+											repeats: YES];  
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
